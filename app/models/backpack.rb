0f26c500a95a6f869ed1675d4c2589fd6f586e11
Denzel Braithwaite <braithwaite.d@hotmail.com>
2022-07-12 12:35:50 -0400

    fixing merge issues

--- app/models/backpack.rb
+++ app/models/backpack.rb
@@ -1,10 +1,12 @@
 class Backpack
-  attr_reader :max_inventory
-  attr_accessor :inventory
+  attr_reader :max_inventory, :name
+  attr_accessor :weapons, :food
 
   def initialize
+    name = 'Simple backpack'
     max_inventory = 10
-    inventory = []
+    weapons = []
+    food = []
   end
 
   def upgrade_backpack
